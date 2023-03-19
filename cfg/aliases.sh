916c7944b095d58d3e29843045f264fab8fbb39c
Abraxas <abr4xa5@protonmail.ch>
2023-03-19 16:09:36 -0400

    Add aliases and functions

---
+++ cfg/aliases.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/env bash
+
+if [ -x "$(command -v exa)" ]; then
+    alias ls="exa"
+    alias la="exa --long --all --group"
+fi
+alias sl="ls"
+
+if [ -x "$(command -v lsd)" ]; then
+    alias lls="lsd"
+    alias lla="lsd --long -A --group-directories-first --human-readable"
+fi
+
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+
+# Intuitive map function
+# For example, to list all directories that contain a certain file:
+# find . -name .gitattributes | map dirname
+alias map="xargs -n1"
+
+# Kill all the tabs in Chrome to free up memory
+# [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
+alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'eth0' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i eth0 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# Mirror stdout to stderr, useful for seeing data going through a pipe
+alias peek='tee >(cat 1>&2)'
+
+alias fzp="fzf --preview 'batcat --color=always {}' --preview-window '~3'"
+
+alias weather='curl wttr.in/'
+#-----------
+
+#Quick Scripts
+
+alias hosts='sudo nano /etc/hosts'
+alias grep_ip='grep -E -o "([0-9]{1,3}[.]){3}[0-9]{1,3}"'
+alias Tools='cd ~/Tools'
+alias fd='fdfind'
+alias hg='history | grep'
+alias l1='ls -t -1'
+alias count='find . -type f | wc -l'
+alias untar='tar -zxvf '
+
+alias myalias='nano ~/.aliases'
+alias sublmyalias='subl ~/.aliases'
+alias src='source ~/.zshrc'
+
+alias c='clear'
+
+# ps greappable
+alias psg="ps aux | grep -v grep | grep -i"
+
+#-----------
+# python venv manual
+alias ve='python3 -m venv ./venv'
+alias va='source ./venv/bin/activate'
+
+#-----------
+# git related
+alias gc='git clone'
+
+alias gs='git status'
+alias gsp='git status --porcelain'
+
+alias gaa='git add -A'
+alias gac='git add .'
+
+alias gcm="git commit -am"
+
+alias gp='git push'
+
+alias gl='git log'
+alias glp='git log --pretty=oneline'
+
+#-----------
+
+# tun0 IP
+alias tun0="ifconfig tun0 | grep 'inet ' | cut -d' ' -f10 | tr -d '\n' | xclip -sel clip"
+
+# Serve http server
+#alias serverup="ip -f inet addr show tun0 | echo \"http://\`grep -Po 'inet \K[\d.]+'\`/\"; pwd; python3 -m http.server 80"
+alias www="python_server"
+
+# show all open ports
+alias openports='netstat -tulanp'
+
+#-----------
+
+# THM vpn access
+alias start-thm='sudo openvpn ~/vpn_configs/thm_Abr4Xa5.ovpn'
+
+#HTB vpn access
+alias start-htb='sudo openvpn ~/vpn_configs/lab_Abr4Xa5.ovpn'
+alias start-htb_endgame='sudo openvpn ~/vpn_configs/endgames_Abr4Xa5.ovpn'
+alias start-htb_fortress='sudo openvpn ~/vpn_configs/fortresses_Abr4Xa5.ovpn'
+alias start-htb_pro='sudo openvpn ~/vpn_configs/pro_labs_Abr4Xa5.ovpn'
+alias start-htb_sp='sudo openvpn ~/vpn_configs/starting_point_Abr4Xa5.ovpn'
+alias start-htb_seasons='sudo openvpn ~/vpn_configs/competitive_Abr4Xa5.ovpn'
+
+## Nmap
+
+alias nse-find="cat /usr/share/nmap/scripts/script.db | grep "
+alias pc='pwncat-cs'
+
+alias ffufdirbust='ffuf_directory_bruteforcing'
+alias wfuzzdir='wfuzz_directory_bruteforcing'
+alias grepjs='grep_js_files'
+
+
+alias bat='batcat'
+alias xc='xclip -sel clip'
+
+# cracking zip files with rockyou.txt
+alias crackzip='fcrackzip -v -u -D -p /usr/share/wordlists/rockyou.txt'
+
+
+
+# gospider to spider the website and proxy through burp
+alias spider='gospider -t 50 --sitemap --robots --proxy http://127.0.0.1:8080 -o gospider_output -s '
+alias spider_noburp='gospider -t 50 --sitemap --robots -s '
+
+# Fix anbox in the box
+# alias anboxlaunch='anbox launch --package=org.anbox.appmgr --component=org.anbox.appmgr.AppViewActivity'
+
+# better WHOIS lookups
+alias whois="whois -h whois-servers.net"
+
+# Fixing shells
+py3_tty_upgrade() {
+    echo "python3 -c 'import pty;pty.spawn(\"/bin/bash\")'" | xclip -sel clip
+}
+alias script_tty_upgrade="echo '/usr/bin/script -qc /bin/bash /dev/null'| xclip -sel clip"
+alias tty_fix="stty raw -echo; fg; reset"
+alias tty_conf="stty -a | sed 's/;//g' | head -n 1 | sed 's/.*baud /stty /g;s/line.*//g' | xclip -sel clip"
+
+# docker
+alias dockershell="docker run --rm -i -t --entrypoint=/bin/bash"
+alias dockershellsh="docker run --rm -i -t --entrypoint=/bin/sh"
+
+
+alias nginxhere='docker run --rm -it -p 80:80 -p 443:443 -v "${PWD}:/srv/data" rflathers/nginxserve'
+alias servepython='docker run --name python_simplehttpserver -d -v "${PWD}":/var/www:ro -p 80:8080 trinitronx/python-simplehttpserver'
+alias pythonhttp='servepython'
+alias webdavhere='docker run --rm -it -p 80:80 -v "${PWD}:/srv/data/share" rflathers/webdav'
+
+## docker msf
+alias metasploitpf='docker run --rm -it -v "${HOME}/.msf4:/home/msf/.msf4" -p 8443-9005:8443-9005 metasploitframework/metasploit-framework ./msfconsole'
+alias msfvenomhere='docker run --rm -it -v "${HOME}/.msf4:/home/msf/.msf4" -v "${PWD}:/data" metasploitframework/metasploit-framework ./msfvenom'
+
+## docker hetty
+## review, doexn't work anymore
+alias runhetty='docker run -v $HOME/.hetty:/root/.hetty -p 8080:8080 ghcr.io/dstotijn/hetty:latest'
+
+## print all docker aliases
+# docker_alias() {
+#     echo "dockershell <containerimage> - Start /bin/bash"
+#     echo "dockershellhere <containerimage> - Start /bin/bash and map current directory"
+#     echo "smbservehere - Start Smb server on port 445 and host current directory"
+#     echo "nginxhere - Host current directory over port 445 and 80"
+#     echo "pythonhttp - docker instance of python http server"
+#     echo "webdavhere - Host over webdav"
+#     echo "metasploitpf - Start metasploit and port forward 8443-9005"
+#     echo "msfvenomhere - Map current directory to msfvenom"
+# }
+
+# Helper functions
+
+## delete empty newlines
+alias delEmptynewline="sed '/^[[:space:]]*$/d'"
+
+## urlEncoding and decoding
+alias urlencode='python3 -c "import sys, urllib.parse as ul;print(ul.quote(sys.argv[1]))"'
+alias urlencode+='python3 -c "import sys, urllib.parse as ul;print(ul.quote_plus(sys.argv[1]))"'
+alias urldecode='python3 -c "import sys, urllib.parse as ul;print(ul.unquote(sys.argv[1]))"'
+# TODO Add lamda to percent encode everything 
+
+# dotfiles aliases
+
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.Dotfiles.git/ --work-tree=$HOME'
+
+dot() {
+    if [[ "$#" -eq 0 ]]; then
+        (
+            cd /
+            for i in $(dotfiles ls-files); do
+                echo -n "$(dotfiles -c color.status=always status "$i" -s | sed "s#$i##")"
+                echo -e "¬/$i¬\e[0;33m$(dotfiles -c color.ui=always log -1 --format="%s" -- "$i")\e[0m"
+            done
+        ) | column -t --separator=¬ -T2
+    else
+        dotfiles "$@"
+    fi
+}
---
+++ cfg/functions.sh
@@ -0,0 +1,232 @@
+#! /usr/bin/env bash
+
+function killp_named() {
+    processName="$1"
+    ps aux | grep "$processName" | grep -v grep | grep -v kill_named | awk '{print $2}' | xargs kill -9
+}
+
+function tre() {
+    tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX
+}
+
+function historygrep() {
+    history | grep "$1" | tr -s ' ' | cut -d' ' -f5-
+}
+
+function cheat() {
+    if [[ "$1" == '-p' ]]; then
+        # -p for a pager option
+        topic="$2"
+        curl -s cheat.sh/"$topic" | bat -p -
+    else
+        topic="$2"
+        curl -s cheat.sh/"$topic"
+    fi
+
+}
+
+#change directory and list content
+function cl() {
+    DIRECTORY="$*"
+    # if no DIR given, go home
+    if [ $# -lt 1 ]; then
+        DIRECTORY=$HOME
+    fi
+    builtin cd "${DIRECTORY}" &&
+        # use your preferred ls command
+        ls -F --color=auto
+}
+
+# mkdir && cd
+mkcd() {
+    mkdir -p -- "$1" &&
+        cd -- "$1" || exit 1
+}
+
+list_ips() {
+    ip a show scope global | awk '/^[0-9]+:/ { sub(/:/,"",$2); iface=$2 } /^[[:space:]]*inet / { split($2, a, "/"); print "[\033[96m" iface"\033[0m] "a[1] }'
+}
+
+function python_server() {
+    list_ips
+    echo "http://"$(ip -br a | grep tun0 | tr -s " " | cut -d" " -f3 | cut -d'/' -f1)"/" | xclip -sel clip
+    echo -e "[\e[96m$(pwd)\e[0m]\e[34m" && echo -en "\e[0m"
+    echo "Files" && ls
+    python3 -m http.server 80
+}
+
+setip() {
+    export IP="$1"
+}
+
+newnote() {
+    cp ~/.templates/notes_template.md ./notes.md
+    subl notes.md
+}
+#-----------
+# Templates
+
+## create bash boilerplate code
+newBash() {
+    cp ~/.templates/bash_template.sh "./${1}"
+    subl "${1}"
+}
+
+## create python request boilerplate code
+newPython_request() {
+    cp ~/.templates/pythonRequests_template.py "./${1}"
+    subl "${1}"
+}
+## create python database command boilerplate code
+newPython_db() {
+    cp ~/.templates/pythonDbCommands_template.py "./${1}"
+    subl "${1}"
+}
+
+function semgrepcheck() {
+    "semgrep --config=p/ci --verbose $1 $2"
+}
+
+function nmap_run() {
+    nmap -p- -Pn -T4 --min-rate 1000 --max-retries 5 "$1" "${@:2}"
+}
+
+# Functions
+
+## gobuster
+function godirbust {
+    gobuster dir -u http://"$1" -w /usr/share/wordlists/dirb/common.txt -e -s "200,301,302,401" -x "php,html,txt" -t 100 -o gobuster-"$1"
+}
+
+function gosubdomain() {
+    gobuster vhost -u http://"$1" -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt -o subdomain-gobuster-"$1"
+}
+
+## FFUF
+
+### ffuf directory bruteforcing
+function ffuf_directory_bruteforcing() {
+    ffuf -c -w /usr/share/seclists/Discovery/Web-Content/common.txt -u http://"${1}"/FUZZ -o ffuf_"${1}".md -of md -t 100 -r "${@:2}"
+}
+
+### ffuf directory bruteforcing recursive
+function ffuf_directory_bruteforcing_recursion() {
+    ffuf -c -w /usr/share/seclists/Discovery/Web-Content/common.txt -u http://"${1}"/FUZZ -o ffuf_"${1}".md -of md -t 100 -recursion "${@:2}"
+}
+function ffuf_onelistforall() {
+    ffuf -c -w /usr/share/wordlists/OneListForAll/onelistforallmicro.txt -u http://"${1}"/FUZZ -t 100 -r -of md -o onelistforallFUZZ.md "${@:2}"
+}
+
+### ffuf subdomain enumeration
+function ffuf_subdomain_enum() {
+    ffuf -c -u http://"$1" -H "Host: FUZZ.${1}" -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt -o ffuf-subdomain_enum-"${1}".md -of md -v "${@:2}"
+}
+
+## wfuzz
+
+### wfuzz directory bruteforcing
+function wfuzz_directory_bruteforcing() {
+    wfuzz -c -w /usr/share/seclists/Discovery/Web-Content/common.txt -f wfuzz_"$1".md -u http://"$1"/FUZZ "${@:2}"
+}
+
+### wfuzz subdomain enum
+function wfuzz_subdomain_enum() {
+    sudo wfuzz -c -Z -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt -H "Host: FUZZ.${1}" -f subdomain_wfuzz_"$1".md -u http://"$1" "${@:2}"
+}
+
+function grep_js_files() {
+    cmd <"$1" | js-beautify | grep "$2"
+}
+
+# nikto with output
+function nicto() {
+    nikto -output nikto-"$1".txt -ask no -url http://"$1"
+}
+
+function nuclei_run() {
+    nuclei -u "${1}" -as -o nuclei_output.txt "${@:2}"
+}
+
+john_rock() {
+    if [ $# -eq 0 ]; then
+        echo "[+] Usage: rock_john wordlist (options)"
+    else
+        john "${@}" --wordlist=/usr/share/wordlists/rockyou.txt
+    fi
+}
+
+# optimized rustscan with nmap script and version scanning
+function rscan() {
+    domain="$1" && shift
+    rustscan -a "$domain" --ulimit 5000 -n -- -sCV -v "$@"
+}
+
+# SSH aliases
+
+## check for authentication type
+function sshAuthCheck() {
+    ssh user@"${1}" -G | grep -E 'pubkeyauthentication|passwordauthentication'
+}
+
+## login with saving host in KnownHostsFile
+function sshLogin() {
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${@}"
+}
+
+# OSINT
+
+## ipinfo
+ipinfo() {
+    curl ipinfo.io/"$1"
+}
+ipinfo_file() {
+    file="$1"
+    for ip in $(cat "$file"); do
+        echo -n "$ip: "
+        curl -s ipinfo.io/"$ip" | jq .org
+    done
+}
+
+## emailrep.io
+email_info() {
+    curl emailrep.io/"${1}"
+}
+
+# docker
+
+dockershellhere() {
+    dirname=${PWD##*/}
+    docker run --rm -it --entrypoint=/bin/bash -v "$(pwd)":/"${dirname}" -w /"${dirname}" "$@"
+}
+dockershellshhere() {
+    dirname=${PWD##*/}
+    docker run --rm -it --entrypoint=/bin/sh -v "$(pwd)":/"${dirname}" -w /"${dirname}" "$@"
+}
+alias impacket_docker="docker run --rm -it rflathers/impacket"
+
+smbservehere() {
+    local sharename
+    [[ -z $1 ]] && sharename="SHARE" || sharename=$1
+    docker run --rm -it -p 445:445 -v "${PWD}:/tmp/serve" rflathers/impacket smbserver.py -smb2support "$sharename" /tmp/serve
+}
+
+# chisel
+function chisel_server_start() {
+
+    echo 'Usage'
+    echo './chisel server --reverse -p LISTEN_PORT'
+    echo './chisel client ATTACKING_IP:LISTEN_PORT R:LOCAL_PORT:TARGET_IP:TARGET_PORT'
+    echo ''
+    echo 'This will create a tunnel to `TARGET_IP:TARGET_PORT` accessible through `127.0.0.1:LOCAL_PORT`'
+    echo ''
+    /home/kali/Tools/chisel/chisel_1.7.6_linux_amd64 server --reverse --port "${1}"
+}
+
+# TODO Improve this
+function xnLinkFinder() {
+    cd /home/kali/Tools/xnLinkFinder || exit 1
+    pipenv run python xnLinkFinder.py "${@}"
+    cd - || exit 1
+    mv "$OLDPWD/output.txt" ./xnLinkFinder_output.txt
+    mv "$OLDPWD/parameters.txt" ./xnLinkFinder_parameters.txt
+}
