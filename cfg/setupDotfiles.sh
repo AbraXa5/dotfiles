a1eac67c3dad383b40215cd081ceef7cf57e36c3
Abraxas <mehul.ingale@gmail.com>
2023-03-20 02:19:30 +0530

    Update setupDotfiles.sh
    Change repository

--- cfg/setupDotfiles.sh
+++ cfg/setupDotfiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone --bare git@github.com:AbraXa5/wslDotfiles $HOME/.wslDotfiles.git
+git clone --bare git@dotfiles.github.com:AbraXa5/dotfiles $HOME/.dotfiles.git
 # define config alias locally
 function dotfiles {  
    git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $@  
@@ -9,7 +9,7 @@ function dotfiles {
 mkdir -p .dotfiles-backup  
 dotfiles checkout  
 if [ $? = 0 ]; then  
-  echo "Checked out dotfiles from git@github.com:AbraXa5/wslDotfiles";  
+  echo "Checked out dotfiles from git@github.com:AbraXa5/.dotfiles";  
   else  
     echo "Moving existing dotfiles to ~/.dotfiles-backup";  
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}  
